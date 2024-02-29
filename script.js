4fdaedbbbbad92ad42fb5cb38631a28576810ca9
VictorVendrameto <vendrametovictor@gmail.com>
2024-02-29 16:02:20 -0300

    Finalizando o backend

--- script.js
+++ script.js
@@ -8,35 +8,45 @@ form.addEventListener('submit', function(event){
 
     const imc = (peso / (altura * altura)).toFixed(2);
 
-    const value = document.getElementById('value');
-    let descricao = '';
 
-    document.getElementById('infos').classList.remove('hidden');
+    if(!isNaN(imc)){
+        const value = document.getElementById('value');
+        let descricao = '';
 
-    if (imc < 18.5)
-    {
-        descricao = "Você está abaixo do peso";
-    }
-        else if (imc >= 18.6 && imc <= 24.9)
+        document.getElementById('infos').classList.remove('hidden');
+    
+    
+        if (imc < 18.5)
         {
-            descricao = "Peso normal";
+            descricao = "Você está abaixo do peso";
         }
-        else if (imc >= 25.0 && imc <= 29.9)
-        {
-            descricao = "Sobrepeso";
+            else if (imc >= 18.6 && imc <= 24.9)
+            {
+                descricao = "Peso normal";
+            }
+            else if (imc >= 25.0 && imc <= 29.9)
+            {
+                descricao = "Sobrepeso";
+            }
+            else if (imc >= 30.0 && imc <= 34.9)
+            {
+                descricao = "Obesidade grau I";
+            }
+            else if (imc >= 35.0 && imc <= 39.9)
+            {
+                descricao = "Obesidade grau II";
+            }
+            else    
+            {
+                descricao = "Obesidade grau III";
+            }
+         value.textContent = imc.replace('.', ',')
+         document.getElementById('descricao').textContent = descricao;
         }
-        else if (imc >= 30.0 && imc <= 34.9)
-        {
-            descricao = "Obesidade grau I";
-        }
-        else if (imc >= 35.0 && imc <= 39.9)
-        {
-            descricao = "Obesidade grau II";
-        }
-        else    
-        {
-            descricao = "Obesidade grau III";
-        }
-    value.textContent = imc.replace('.', ',')
-    document.getElementById('descricao').textContent = descricao;
 } );
+
+form.addEventListener('keypress', function(event){
+    if (event.key === 'press'){
+        document.getElementById('#calcular').click();
+    }
+});
